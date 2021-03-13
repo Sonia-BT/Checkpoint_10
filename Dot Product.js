ee0241542d3d45f69ca5d2a280423e64e9061139
bouta <boutaghousonia11@gmail.com>
2021-03-13 07:33:34 +0100

    Orthogonal

--- Dot Product.js
+++ Dot Product.js
@@ -1,5 +1,4 @@
-let v1 = [3, 2.5, 2, 0],
-  v2 = [-2, 2, 0.5, 0];
+/********************Produit vectoriels + Orthogonal***********************/
 let obj = {
   Product: 0,
 };
@@ -9,14 +8,18 @@ function dot_product(v1, v2, obj) {
   }
 }
 
-dot_product(v1, v2, obj);
-let x = dot_product(v1, v2, obj).Product;
-function Orthogonal(dot_product) {
-  if (dot_product.Product == 0) {
+function Orthogonal() {
+  dot_product(v1, v2, obj);
+  if (obj.Product == 0) {
     return "Orthogonal true";
   } else {
     return "Orthogonal false";
   }
 }
 
-Orthogonal(dot_product(v1, v2));
+let v1 = [3, 2.5, 2, 0],
+  v2 = [-2, 2, 0.5, 0],
+  v3 = [-2, 2, 0.5, 1],
+  v4 = [3, 2.5, 2, -1];
+Orthogonal();
+console.log(Orthogonal(dot_product(v1, v2, obj))); //v3 et v4 false
